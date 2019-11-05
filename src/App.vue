55f09048a7af9c832844425a91643fa4e9f41b13
anudeike <ikechukwuanude@gmail.com>
2019-11-04 20:21:28 -0500

    added the icon set and fully configured vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <template>
   <v-app>
     <v-app-bar app>
+
+      <!--This the toolbar that will be present on each page-->
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <v-avatar color="indigo" size="36" class="mr-4">
+          <img
+            src="https://res.cloudinary.com/cheezitromansh/image/upload/v1572915189/kaleidoscope/Untitled-Artwork_2_aqamnh.jpg"
+            alt="logo"
+          />
+        </v-avatar>
+
+        <span>Kaleido</span>
+        <span class="font-weight-light">scope</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
       <v-btn
         text
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
