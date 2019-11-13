00492e639ebb586079a9b69c3c7ae87731d34b24
anudeike <ikechukwuanude@gmail.com>
2019-11-13 16:36:09 -0500

    created homepage

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,7 +5,18 @@
 
             </v-flex>
             <v-flex class="text-center">
-                {{ myData }}
+                <span><p class="display-4 white--text shadow"> Kaleidoscope </p></span>
+                <v-text-field
+                        label="enter a term"
+                        color="white"
+                        v-model="searchTerm"
+                        filled
+                        clearable
+                        required>
+
+                </v-text-field>
+
+                <v-btn x-large color="purple darken-3" dark>Search</v-btn>
             </v-flex>
             <v-flex class="text-center placeholder">
 
@@ -19,11 +30,15 @@ export default {
   data() {
       return {
           myData: "home page",
+          searchTerm: ""
       }
   }
 };
 </script>
 
-<style scoped>
+<style>
+    p.shadow {
+        text-shadow: 2px 2px 10px rgba(0,0,0,0.4);
+    }
 
 </style>
