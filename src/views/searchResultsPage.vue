561f26987479c957e489b3c464bcc1d9eb7d50c5
anudeike <ikechukwuanude@gmail.com>
2019-12-04 06:36:57 -0500

    got information and specific palettes

--- src/views/searchResultsPage.vue
+++ src/views/searchResultsPage.vue
@@ -10,6 +10,19 @@
                     </v-card-text>
                 </v-card>
             </v-flex>
+            <v-flex>
+                <v-container fill-height>
+                    <v-layout row>
+                        <v-flex md2 v-for="s in someData" :key="s">
+                            <v-card>
+                                <v-card-text>
+                                    {{ s }} something
+                                </v-card-text>
+                            </v-card>
+                        </v-flex>
+                    </v-layout>
+                </v-container>
+            </v-flex>
         </v-layout>
     </v-container>
 </template>
@@ -24,16 +37,28 @@
         },
         data (){
             return {
-                someData: [2,2,2,2,2,2,2,2],
-                paletteResults: null
+                someData: [2,2,2,2,2,2,2,2,2,2,2],
+                paletteResults: null,
+                keys:[],
+                targetPalettes: [],
+                query: ""
             }
         },
         mounted() {
+            this.query = this.$route.params.query;
+
             // get the posts from firebase
             this.$http.get('https://kaleidoscope-app-92131.firebaseio.com/palettes.json').then((data) => {
                 return data.json(); // returns a promise to use below
             }).then((data) => {
                 this.paletteResults = data;
+
+                // append the keys - get the targeted palettes
+                for (let key in data){
+                    if(data[key].tags.includes(this.query)){
+                        this.targetPalettes.push(data[key]);
+                    }
+                }
             })
 
         }
